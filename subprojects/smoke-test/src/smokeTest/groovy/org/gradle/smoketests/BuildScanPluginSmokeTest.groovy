b46c4f43c6f700f411d97cab7c474e82fadf79e7
Rene Groeschke <rene@breskeby.com>
2018-11-12 16:46:30 +0100

    Decororate taskcontainer callbacks to track application id

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
@@ -50,6 +50,7 @@ class BuildScanPluginSmokeTest extends AbstractSmokeTest {
     @Unroll
     "gracefully succeeds without capturing scan with unsupported version #version"() {
         when:
+
         usePluginVersion version
 
         and:
